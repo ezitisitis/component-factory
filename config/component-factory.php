50fbeb38c05814cd4bfb12bacbe5272512ed9f85
Marks Bogdanovs <git@ezitisitis.com>
2021-05-04 23:11:32 +0300

    Add env to config file

--- config/component-factory.php
+++ config/component-factory.php
@@ -3,12 +3,12 @@
 return [
     'path' => [
         'component' => [
-            'view'  => resource_path('views/components'),
-            'style' => resource_path('assets/scss/components'),
+            'view'  => resource_path(env('COMPONENT_FACTORY_COMPONENT_VIEW_PATH', 'views/components')),
+            'style' => resource_path(env('COMPONENT_FACTORY_COMPONENT_STYLE_PATH', 'assets/scss/components')),
         ],
         'element' => [
-            'view'  => resource_path('views/elements'),
-            'style' => resource_path('assets/scss/elements'),
+            'view'  => resource_path(env('COMPONENT_FACTORY_ELEMENT_VIEW_PATH', 'views/elements')),
+            'style' => resource_path(env('COMPONENT_FACTORY_ELEMENT_STYLE_PATH', 'assets/scss/elements')),
         ],
     ],
 ];
